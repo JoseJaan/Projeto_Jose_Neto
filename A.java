cf9c1b81a779aa1b8cb812b89cfcd47507e587da
Jose Acerbi Almeida Neto <jose@neto.neto>
2024-10-16 10:44:50 -0300

    feat: adicionando novas classes

---
+++ A.java
@@ -0,0 +1,33 @@
+public class A {
+    int A1;
+    float A2;
+
+    public A(){
+        A1 = 0;
+        A2 = 0.0f;
+    }
+
+    public int getA1(){
+        return A1;
+    }
+
+    public void setA1(int value){
+        A1 = value;
+    }
+
+    public float getA2(){
+        return A2;
+    }
+
+    public void setA2(float value){
+        A2 = value;
+    }
+
+    public void MA1(){
+        System.out.println("MA1");
+    }
+
+    public void MA2(){
+        System.out.println("MA2");
+    }
+}
---
+++ B.java
@@ -0,0 +1,33 @@
+public class B {
+    int B1;
+    float B2;
+
+    public B(){
+        B1 = 0;
+        B2 = 0.0f;
+    }
+
+    public int getB1(){
+        return B1;
+    }
+
+    public void setB1(int value){
+        B1 = value;
+    }
+
+    public float getB2(){
+        return B2;
+    }
+
+    public void setB2(float value){
+        B2 = value;
+    }
+
+    public void MB1(){
+        System.out.println("MB1");
+    }
+
+    public void MB2(){
+        System.out.println("MB2");
+    }
+}
